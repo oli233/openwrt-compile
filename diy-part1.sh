5a3c052ee9667710f05605dad1bfa9060a332655
Zhiwei Xin <34482354+oli233@users.noreply.github.com>
2026-05-08 15:25:24 +0800

    Update diy-part1.sh fix ttyd

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git ttyd https://github.com/ozon/luci-app-ttyd' >>feeds.conf.default
